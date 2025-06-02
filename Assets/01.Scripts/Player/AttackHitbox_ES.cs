d8b11177ac843d5d5722f7d240daf3dddd9ef877
redyeowoo <seungwoo9621@gmail.com>
2025-06-02 23:51:01 +0900

    docs: 적이 피격 안되던 거 고침

--- Assets/01.Scripts/Player/AttackHitbox_ES.cs
+++ Assets/01.Scripts/Player/AttackHitbox_ES.cs
@@ -4,14 +4,31 @@ public class AttackHitbox : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        // 충돌한 콜라이더가 부모 오브젝트를 가지고 있는지 체크
+        if (other.transform.parent != null)
         {
-            EnemyHealthUI enemyHealth = other.GetComponentInChildren<EnemyHealthUI>();
-            if (enemyHealth != null)
+            GameObject parentObject = other.transform.parent.gameObject;
+
+            // 부모 오브젝트가 "Enemy" 태그인지 확인
+            if (parentObject.CompareTag("Enemy"))
             {
-                enemyHealth.TakeDamage(20f); // 데미지 값은 상황에 맞게 조정
+                EnemyHealthUI enemyHealth = parentObject.GetComponentInChildren<EnemyHealthUI>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(20f);
+                }
+            }
+        }
+        else
+        {
+            if (other.CompareTag("Enemy"))
+            {
+                EnemyHealthUI enemyHealth = other.GetComponentInChildren<EnemyHealthUI>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.TakeDamage(20f);
+                }
             }
         }
     }
 }
-
