e690606c41e195fc33ab3347eceb5c20d30a1b71
박민규 <pmq0101@naver.com>
2025-07-13 17:10:25 +0900

    feat: 단서 조각구분을 위한 clueIndex 추가

--- Assets/01.Scripts/ClueBox/ClueInfos.cs
+++ Assets/01.Scripts/ClueBox/ClueInfos.cs
@@ -7,4 +7,6 @@ public class ClueInfos : ScriptableObject
     public string name;
     public string description;
     public Sprite clueImage;
+    // clueIndex가 0이면 완성 단서
+    public int clueIndex;
 }
