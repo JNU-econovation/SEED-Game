9af2cc9d43188a5f7940adb06e441ad8dfc1ccf5
박민규 <pmq0101@naver.com>
2025-07-13 18:34:41 +0900

    feat: 단서 합치기 구현

--- Assets/01.Scripts/ClueBox/ClueBox.cs
+++ Assets/01.Scripts/ClueBox/ClueBox.cs
@@ -19,4 +19,31 @@ public class ClueBox : MonoBehaviour
     {
         clueInfosSet.Remove(clue);   
     }
+    
+    public void MergeClue()
+    {
+        Dictionary<ClueInfos, List<ClueInfos>> clueDict = new ();
+        foreach (ClueInfos clue in clueInfosSet)
+        {
+            // 완성 단서면 합칠 필요 없음
+            if (clue.clueIndex == 0) continue;
+            if (!clueDict.ContainsKey(clue.completeClue))
+            {
+                clueDict.Add(clue.completeClue, new List<ClueInfos>());
+            }
+            clueDict[clue.completeClue].Add(clue);
+        }
+
+        foreach (var (completeClueInfos, pieces) in clueDict)
+        {
+            if (pieces.Count == 4)
+            {
+                foreach (ClueInfos p in pieces)
+                {
+                    RemoveClue(p);
+                }
+                AddClue(completeClueInfos);
+            }
+        }
+    }
 }
