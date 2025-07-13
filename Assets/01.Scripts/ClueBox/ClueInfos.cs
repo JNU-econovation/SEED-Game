dfc60b5f8e272ea67408bd49e40f5288183848f0
박민규 <pmq0101@naver.com>
2025-07-13 17:56:03 +0900

    feat: 단서 조각품이 완성 단서를 참조하게 추가

--- Assets/01.Scripts/ClueBox/ClueInfos.cs
+++ Assets/01.Scripts/ClueBox/ClueInfos.cs
@@ -9,4 +9,6 @@ public class ClueInfos : ScriptableObject
     public Sprite clueImage;
     // clueIndex가 0이면 완성 단서
     public int clueIndex;
+    // 조각 단서일 때 참조할 단서
+    public ClueInfos completeClue;
 }
